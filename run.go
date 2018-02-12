8f62e54b5b71e1872e225c5c48c83c06bdb174f8
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-02-12 23:13:18 +0100

    added run fn

---
+++ run.go
@@ -0,0 +1,35 @@
+package system
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Run(fn func(kill <-chan struct{}) error) error {
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan error, 1)
+	kill := make(chan struct{}, 1)
+
+	defer close(sigs)
+	defer close(done)
+	defer close(kill)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	defer signal.Stop(sigs)
+
+	go func() {
+		done <- fn(kill)
+	}()
+
+	//return <-done
+	select {
+	case err := <-done:
+		return err
+	case <-sigs:
+		kill <- struct{}{}
+		break
+	}
+	return nil
+}
